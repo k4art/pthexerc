9ea4e8af5851d965111703a5315fdf660739f9a9
Artem Kan <kan.art.email@gmail.com>
2023-01-11 15:17:39 +0500

    feat: support LOG_WORKS macro and improve output

--- thread_pool/main.c
+++ thread_pool/main.c
@@ -8,15 +8,19 @@
 static const size_t THREADS_NUMBER    = 8;
 static const size_t WORK_ITEMS_NUMBER = 1000;
 
+static const int VALUE_INCREMENT = 1000;
+
 static void work_routine(void * arg)
 {
   int * value = arg;
   int   old   = *value;
 
-  *value += 1000;
+  *value += VALUE_INCREMENT;
 
+#if defined(LOG_WORKS)
   // Not all logs might appear in stdout due to lack of synchronization
   printf("tid=%lu, old=%d, val=%d\n", pthread_self(), old, *value);
+#endif
 
   if (*value % 2 != 0)
   {
@@ -36,6 +40,25 @@ int main(void)
   }
 
   tpool_wait(tpool);
+
+  bool failed = false;
+  for (size_t i = 0; i < WORK_ITEMS_NUMBER; i++)
+  {
+    if (values[i] != i + VALUE_INCREMENT)
+    {
+      fprintf(stderr, "[FAILURE]: values[%zu] = %d", i, values[i]);
+      failed = true;
+    }
+  }
+
+  if (failed)
+  {
+    printf("\nFailures detected.\n");
+  }
+  else
+  {
+    printf("\nAll works are completed!\n");
+  }
 
   free(values);
   tpool_destroy(tpool);
