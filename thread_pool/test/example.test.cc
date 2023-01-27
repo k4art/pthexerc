bf48bd04ca462cf5dd875611e02df820399d63e3
Artem Kan <kan.art.email@gmail.com>
2023-01-27 20:28:36 +0500

    test(thread_pool): write simple tests for work_queue
    - delete test/example.test.cc

--- thread_pool/test/example.test.cc
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(ExampleSuite, ExampleTestCase)
-{
-  EXPECT_EQ(1, 1);
-}
-
---
+++ thread_pool/test/work_queue.test.cc
@@ -0,0 +1,62 @@
+#include "gtest/gtest.h"
+
+extern "C"
+{
+  #include "work_queue.h"
+}
+
+bool operator == (const work_t & lhs, const work_t & rhs)
+{
+  bool same_routine = lhs.routine == rhs.routine;
+  bool same_arg     = lhs.arg == rhs.arg;
+
+  return same_routine && same_arg;
+}
+
+static void dummy_work_routine(void * arg)
+{
+  // nothing
+}
+
+#define DUMMY_WORK                     \
+{ .routine = dummy_work_routine,       \
+  .arg = (void *) dummy_work_routine }
+
+TEST(WorkQueue, keeping_single_element)
+{
+  work_t temp;
+  const work_t work = DUMMY_WORK;
+
+  work_queue_t * queue = work_queue_create(3);
+
+  for (size_t i = 0; i < 7; i++)
+  {
+    work_queue_add(queue, &work);
+    work_queue_remove(queue, &temp);
+
+    EXPECT_EQ(work, temp);
+  }
+}
+
+TEST(WorkQueue, keeping_many_elements)
+{
+  work_t temp;
+  const work_t work = DUMMY_WORK;
+
+  work_queue_t * queue = work_queue_create(10);
+
+  for (size_t tries = 0; tries < 5; tries++)
+  {
+    for (size_t i = 0; i < 3; i++)
+    {
+      work_queue_add(queue, &work);
+    }
+
+    for (size_t i = 0; i < 3; i++)
+    {
+      work_queue_remove(queue, &temp);
+      EXPECT_EQ(work, temp);
+    }
+  }
+}
+
